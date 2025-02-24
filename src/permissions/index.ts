c4159dbfc77b6674d4680538d52ae7ee5432800c
codinnovate <adeyemis710@gmail.com>
2025-02-24 11:12:00 +0100

    feat: add permission system barrel exports

---
+++ src/permissions/index.ts
@@ -0,0 +1,11 @@
+export type {
+  PermissionAction,
+  PermissionRequest,
+  PermissionDecision,
+  PermissionChecker,
+  StoredPermission,
+} from './types.js';
+
+export { PermissionStore } from './store.js';
+export { PermissionManager } from './manager.js';
+export type { PermissionManagerOptions } from './manager.js';
